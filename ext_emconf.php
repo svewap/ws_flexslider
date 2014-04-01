b2e1711e6912f019fc575b7bd9b1b9ea796786f6
Xaver Maierhofer <xmaierhofer@1drop.de>
2014-04-01 15:39:08 +0200

    TYPO3 6.2 compatibly - replaced makeInstance with objectManager

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'extbase' => '4.7',
 			'fluid' => '4.7',
-			'typo3' => '4.7.0-6.1.99',
+			'typo3' => '4.7.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
