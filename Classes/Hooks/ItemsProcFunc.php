b1228caeb13fede0d50cbc5ae68d12c1d1920246
Sven Wappler <info@wappler.systems>
2016-09-07 09:02:11 +0200

    Pushed missed files

---
+++ Classes/Hooks/ItemsProcFunc.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace WapplerSystems\WsFlexslider\Hooks;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use WapplerSystems\WsFlexslider\Utility\TemplateLayout;
+use TYPO3\CMS\Backend\Utility\BackendUtility as BackendUtilityCore;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Userfunc to render alternative label for media elements
+ *
+ */
+class ItemsProcFunc
+{
+
+    /** @var TemplateLayout $templateLayoutsUtility */
+    protected $templateLayoutsUtility;
+
+    public function __construct()
+    {
+        $this->templateLayoutsUtility = GeneralUtility::makeInstance(TemplateLayout::class);
+    }
+
+    /**
+     * Itemsproc function to extend the selection of templateLayouts in the plugin
+     *
+     * @param array &$config configuration array
+     * @return void
+     */
+    public function user_templateLayout(array &$config)
+    {
+        $row = $this->getContentElementRow($config['row']['uid']);
+
+        $templateLayouts = $this->templateLayoutsUtility->getAvailableTemplateLayouts($row['pid']);
+        foreach ($templateLayouts as $layout) {
+            $additionalLayout = [
+                $this->getLanguageService()->sL($layout[0], true),
+                $layout[1]
+            ];
+            array_push($config['items'], $additionalLayout);
+        }
+    }
+
+
+    /**
+     * Get tt_content record
+     *
+     * @param int $uid
+     * @return array
+     */
+    protected function getContentElementRow($uid)
+    {
+        return BackendUtilityCore::getRecord('tt_content', $uid);
+    }
+
+    /**
+     * Returns LanguageService
+     *
+     * @return \TYPO3\CMS\Lang\LanguageService
+     */
+    protected function getLanguageService()
+    {
+        return $GLOBALS['LANG'];
+    }
+
+}
---
+++ Classes/Utility/TemplateLayout.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace WapplerSystems\WsFlexslider\Utility;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * TemplateLayout utility class
+ */
+class TemplateLayout implements SingletonInterface
+{
+
+    /**
+     * Get available template layouts for a certain page
+     *
+     * @param int $pageUid
+     * @return array
+     */
+    public function getAvailableTemplateLayouts($pageUid)
+    {
+        $templateLayouts = [];
+
+        // Check if the layouts are extended by ext_tables
+        if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['ws_flexslider']['templateLayouts'])
+            && is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['ws_flexslider']['templateLayouts'])
+        ) {
+            $templateLayouts = $GLOBALS['TYPO3_CONF_VARS']['EXT']['ws_flexslider']['templateLayouts'];
+        }
+
+        // Add TsConfig values
+        foreach ($this->getTemplateLayoutsFromTsConfig($pageUid) as $templateKey => $title) {
+            if (GeneralUtility::isFirstPartOfStr($title, '--div--')) {
+                $optGroupParts = GeneralUtility::trimExplode(',', $title, true, 2);
+                $title = $optGroupParts[1];
+                $templateKey = $optGroupParts[0];
+            }
+            $templateLayouts[] = [$title, $templateKey];
+        }
+
+        return $templateLayouts;
+    }
+
+    /**
+     * Get template layouts defined in TsConfig
+     *
+     * @param $pageUid
+     * @return array
+     */
+    protected function getTemplateLayoutsFromTsConfig($pageUid)
+    {
+        $templateLayouts = [];
+        $pagesTsConfig = BackendUtility::getPagesTSconfig($pageUid);
+        if (isset($pagesTsConfig['tx_wsflexslider.']['templateLayouts.']) && is_array($pagesTsConfig['tx_wsflexslider.']['templateLayouts.'])) {
+            $templateLayouts = $pagesTsConfig['tx_wsflexslider.']['templateLayouts.'];
+        }
+        return $templateLayouts;
+    }
+}
