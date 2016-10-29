35e61c984485a7adba3d8e8870a51d8911555c1c
Sven Wappler <info@wappler.systems>
2016-10-30 00:37:46 +0200

    Now compatible to 8.4

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,35 +1,41 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'WapplerSystems.' . $_EXTKEY,
-	'Pi1',
-	array(
-		'Flexslider' => 'list',
-	),
-	// non-cacheable actions
-	array(
-		
-	)
-);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ws_flexslider/Configuration/TSconfig/ContentElementWizard.txt">');
+call_user_func(
+    function ($extKey) {
 
-
-if (TYPO3_MODE === 'BE') {
-    $icons = [
-        'ext-wsflexslider-wizard-icon' => 'plugin_wizard.png',
-        'ext-wsflexslider-image' => 'picture.png',
-    ];
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    foreach ($icons as $identifier => $path) {
-        $iconRegistry->registerIcon(
-            $identifier,
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:ws_flexslider/Resources/Public/Icons/' . $path]
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'WapplerSystems.' . $extKey,
+            'Pi1',
+            array(
+                'Flexslider' => 'list',
+            ),
+            // non-cacheable actions
+            array()
         );
-    }
-}
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ws_flexslider/Configuration/TSconfig/ContentElementWizard.txt">');
+
+
+        if (TYPO3_MODE === 'BE') {
+            $icons = [
+                'ext-wsflexslider-wizard-icon' => 'plugin_wizard.png',
+                'ext-wsflexslider-image' => 'picture.png',
+            ];
+            $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+            foreach ($icons as $identifier => $path) {
+                $iconRegistry->registerIcon(
+                    $identifier,
+                    \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+                    ['source' => 'EXT:'.$extKey.'/Resources/Public/Icons/' . $path]
+                );
+            }
+        }
+
+    },
+    'ws_flexslider'
+);
 
--- ext_tables.php
+++ ext_tables.php
@@ -1,33 +1,40 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
-// Build extension name vars - used for plugin registration, flexforms and similar
-$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
-$pluginSignature = strtolower($extensionName) . '_pi1';
+call_user_func(
+    function ($extKey) {
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Pi1',
-	'WS Flexslider'
+        // Build extension name vars - used for plugin registration, flexforms and similar
+        $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey);
+        $pluginSignature = strtolower($extensionName) . '_pi1';
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Pi1',
+            'WS Flexslider'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript',
+            'Flexslider');
+
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_wsflexslider_domain_model_image');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+            'tx_wsflexslider_domain_model_image',
+            'EXT:ws_flexslider/Resources/Private/Language/locallang_csh_image.xlf');
+
+
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'tx_wsflexslider_images,pi_flexform';
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature,
+            'FILE:EXT:' . $extKey . '/Configuration/FlexForm/Flexslider.xml');
+
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($extKey, 'Pi1', 'Flexslider');
+
+    },
+    'ws_flexslider'
 );
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Flexslider');
-
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_wsflexslider_domain_model_image');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_wsflexslider_domain_model_image', 'EXT:ws_flexslider/Resources/Private/Language/locallang_csh_image.xlf');
-
-
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'tx_wsflexslider_images,pi_flexform';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForm/Flexslider.xml');
-
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($_EXTKEY, 'Pi1', 'Flexslider');
-
-
