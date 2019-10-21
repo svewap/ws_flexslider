e7c1a11e2d141ba7a43dd9d450d151ea6f17d42f
Sven Wappler <info@wappler.systems>
2019-10-21 16:52:53 +0200

    Bugfix: Key deleted

--- ext_tables.sql
+++ ext_tables.sql
@@ -33,7 +33,6 @@ CREATE TABLE tx_wsflexslider_domain_model_image (
 
 	PRIMARY KEY (uid),
 	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
 	KEY language (l10n_parent,sys_language_uid)
 
 );
