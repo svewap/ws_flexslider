74afdbfb62fb12d0c49c158d993be308ac8704f7
Sven Wappler <info@wappler.systems>
2020-05-09 18:22:30 +0200

    new version 1.5.13

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,31 +11,31 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'Flexslider',
-	'description' => 'jQuery Flexslider slideshow script',
-	'category' => 'plugin',
-	'author' => 'Sven Wappler',
-	'author_email' => 'typo3YYYY@wappler.systems',
-	'author_company' => 'WapplerSystems',
-	'shy' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '1.5.12',
-	'constraints' => [
-		'depends' => [
-			'typo3' => '8.7.0-10.4.99',
+    'title' => 'Flexslider',
+    'description' => 'jQuery Flexslider slideshow script',
+    'category' => 'plugin',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.5.13',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-10.4.99',
         ],
-		'conflicts' => [
+        'conflicts' => [
         ],
-		'suggests' => [
+        'suggests' => [
         ],
     ],
-	'_md5_values_when_last_written' => 'a:33:{s:12:"ext_icon.gif";s:4:"d265";s:17:"ext_localconf.php";s:4:"fcff";s:14:"ext_tables.php";s:4:"1155";s:14:"ext_tables.sql";s:4:"2223";s:19:"Classes/Wizicon.php";s:4:"5310";s:43:"Classes/Controller/FlexsliderController.php";s:4:"100a";s:30:"Classes/Domain/Model/Image.php";s:4:"3d6a";s:45:"Classes/Domain/Repository/ImageRepository.php";s:4:"cf09";s:23:"Classes/Utility/Div.php";s:4:"03d3";s:41:"Classes/ViewHelpers/DynLinkViewHelper.php";s:4:"a5ec";s:68:"Classes/ViewHelpers/PageRenderer/AddJsFooterInlineCodeViewHelper.php";s:4:"19f3";s:37:"Configuration/FlexForm/Flexslider.xml";s:4:"829e";s:27:"Configuration/TCA/Image.php";s:4:"eedd";s:38:"Configuration/TypoScript/constants.txt";s:4:"5a72";s:34:"Configuration/TypoScript/setup.txt";s:4:"2c1d";s:40:"Resources/Private/Language/locallang.xml";s:4:"2e5f";s:84:"Resources/Private/Language/locallang_csh_tx_wsflexslider_domain_model_flexslider.xml";s:4:"6ece";s:79:"Resources/Private/Language/locallang_csh_tx_wsflexslider_domain_model_image.xml";s:4:"4d69";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"63ee";s:38:"Resources/Private/Layouts/Default.html";s:4:"41a4";s:48:"Resources/Private/Partials/Flexslider/Entry.html";s:4:"d9cf";s:48:"Resources/Private/Templates/Flexslider/List.html";s:4:"c7d1";s:29:"Resources/Public/Css/demo.css";s:4:"ee89";s:35:"Resources/Public/Css/flexslider.css";s:4:"37c3";s:31:"Resources/Public/Icons/icon.png";s:4:"8c1c";s:34:"Resources/Public/Icons/picture.png";s:4:"d204";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:66:"Resources/Public/Icons/tx_wsflexslider_domain_model_flexslider.gif";s:4:"1103";s:61:"Resources/Public/Icons/tx_wsflexslider_domain_model_image.gif";s:4:"1103";s:44:"Resources/Public/Images/bg_direction_nav.png";s:4:"14ae";s:48:"Resources/Public/JavaScript/jquery.flexslider.js";s:4:"7436";s:57:"Resources/Public/JavaScript/jquery.flexslider.textmode.js";s:4:"ea09";s:14:"doc/manual.sxw";s:4:"5621";}',
+    '_md5_values_when_last_written' => 'a:33:{s:12:"ext_icon.gif";s:4:"d265";s:17:"ext_localconf.php";s:4:"fcff";s:14:"ext_tables.php";s:4:"1155";s:14:"ext_tables.sql";s:4:"2223";s:19:"Classes/Wizicon.php";s:4:"5310";s:43:"Classes/Controller/FlexsliderController.php";s:4:"100a";s:30:"Classes/Domain/Model/Image.php";s:4:"3d6a";s:45:"Classes/Domain/Repository/ImageRepository.php";s:4:"cf09";s:23:"Classes/Utility/Div.php";s:4:"03d3";s:41:"Classes/ViewHelpers/DynLinkViewHelper.php";s:4:"a5ec";s:68:"Classes/ViewHelpers/PageRenderer/AddJsFooterInlineCodeViewHelper.php";s:4:"19f3";s:37:"Configuration/FlexForm/Flexslider.xml";s:4:"829e";s:27:"Configuration/TCA/Image.php";s:4:"eedd";s:38:"Configuration/TypoScript/constants.txt";s:4:"5a72";s:34:"Configuration/TypoScript/setup.txt";s:4:"2c1d";s:40:"Resources/Private/Language/locallang.xml";s:4:"2e5f";s:84:"Resources/Private/Language/locallang_csh_tx_wsflexslider_domain_model_flexslider.xml";s:4:"6ece";s:79:"Resources/Private/Language/locallang_csh_tx_wsflexslider_domain_model_image.xml";s:4:"4d69";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"63ee";s:38:"Resources/Private/Layouts/Default.html";s:4:"41a4";s:48:"Resources/Private/Partials/Flexslider/Entry.html";s:4:"d9cf";s:48:"Resources/Private/Templates/Flexslider/List.html";s:4:"c7d1";s:29:"Resources/Public/Css/demo.css";s:4:"ee89";s:35:"Resources/Public/Css/flexslider.css";s:4:"37c3";s:31:"Resources/Public/Icons/icon.png";s:4:"8c1c";s:34:"Resources/Public/Icons/picture.png";s:4:"d204";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:66:"Resources/Public/Icons/tx_wsflexslider_domain_model_flexslider.gif";s:4:"1103";s:61:"Resources/Public/Icons/tx_wsflexslider_domain_model_image.gif";s:4:"1103";s:44:"Resources/Public/Images/bg_direction_nav.png";s:4:"14ae";s:48:"Resources/Public/JavaScript/jquery.flexslider.js";s:4:"7436";s:57:"Resources/Public/JavaScript/jquery.flexslider.textmode.js";s:4:"ea09";s:14:"doc/manual.sxw";s:4:"5621";}',
 ];
